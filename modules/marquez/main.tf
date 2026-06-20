eafbde5a117c74dbdc09932fbe49fbadbccd4272
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-06-21 00:39:59 +0300

    update

--- modules/marquez/main.tf
+++ modules/marquez/main.tf
@@ -1,1 +1,14 @@
-xxxxx
+
+resource "helm_release" "marquez" {
+  name             = "marquez"
+  repository       = "https://marquezproject.github.io/marquez"
+  chart            = "marquez"
+  namespace        = "marquez"
+  create_namespace = true
+
+  timeout = 600
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+}
